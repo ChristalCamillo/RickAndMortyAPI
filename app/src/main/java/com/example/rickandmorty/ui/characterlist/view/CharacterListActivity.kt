3f2e495b6aad332797ada345d9e112a2aea172bf
ChristalCamillo <christal.b.camillo@gmail.com>
2022-11-04 12:49:59 -0300

    splash activity and viewstate completed on ui level

---
+++ app/src/main/java/com/example/rickandmorty/ui/characterlist/view/CharacterListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rickandmorty.ui.characterlist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_list)
+    }
+}
